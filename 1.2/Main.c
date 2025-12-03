f39a41eb50ee17d0531acff4a70a9765d33507de
DevK <60959635+Sorawi91@users.noreply.github.com>
2025-12-03 11:34:31 +0700

    Add files via upload

---
+++ 1.2/Main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//show function
+void GetSet(int a[], int *n);
+
+int main(){
+    int *data , num;
+    GetSet(data , &num);
+    return 0;
+}
+
+void GetSet(int a[], int *n) {
+    int i;
+
+    printf("Pls Fill Number : ");
+
+    scanf("%d",n);
+
+    for (i = 0; i < *n; i++) {
+        printf("Show Row %d : ",i+1);
+
+        scanf("%d",&a[i]);
+    }
+    
+    
+}
