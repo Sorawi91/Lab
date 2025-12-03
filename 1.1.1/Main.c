37f988c6bc82f09f95b057f7eee2592f67702112
DevK <60959635+Sorawi91@users.noreply.github.com>
2025-12-03 18:19:44 +0700

    Add files via upload

---
+++ 1.1.1/Main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Fill Number in textbox for print(Hello world): ");
+
+    if (scanf("%d", &n) != 1)
+    {
+        return 1;
+    }
+
+    if (n <= 0)
+    {
+        printf("Error Pls Fill above (1-n)");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("[%d]", i + 1);
+        printf(" Hello world\n");
+    }
+}
