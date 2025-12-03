1f6ac014337cd8348f22199bd6eeb5ea97a654e8
DevK <60959635+Sorawi91@users.noreply.github.com>
2025-12-03 18:01:52 +0700

    Delete 1.4 directory

--- 1.4/Main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void GetSet(int ptrdata[],int *number);
-
-int main(){ int data[50],num;GetSet(data,&num);return 0; }
-
-void GetSet(int ptrdata[],int *number){ printf("Plsese Fill Number For Member you need to input data: "); scanf("%d",number); for(int i = 0 ; i < *number ; i++){ printf("Plsese Fill data for member [%d] : ",i+1); scanf("%d",ptrdata[i]); }
-}
