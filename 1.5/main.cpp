d337811b420f3434e1d8bf78fcdca5d59579a054
DevK <60959635+Sorawi91@users.noreply.github.com>
2025-12-05 01:29:48 +0700

    Add files via upload

---
+++ 1.5/main.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void GetMatrix(int **value ,int *row ,int *col);
+int main(){ int *arr,m,n; GetMatrix(&arr,&m,&n); return 0; }
+void GetMatrix(int **value ,int *row ,int *col){ printf("Plsese Fill row and col: "); scanf("%d %d",row,col); int size = (*row) * (*col); *value = new int[size]; printf("Plsese Fill %d number\n",size ); for(int i = 0 ; i < size ; i++){ printf("[%d]: ",i+1); scanf("%d",&value[i]); } }
+
