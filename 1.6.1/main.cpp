ec65453cabc276f6e6312929a4faf82cd12e077d
DevK <60959635+Sorawi91@users.noreply.github.com>
2025-12-05 01:52:16 +0700

    Add files via upload

---
+++ 1.6.1/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int GetMatrix(int **ar, int *row, int *col);
+int main()
+{
+    int *data, m, n;
+    GetMatrix(&data, &m, &n);
+    return 0;
+}
+int GetMatrix(int **ar, int *row, int *col)
+{
+    printf("Fill row and col pls: ");
+    scanf("%d %d", &(*row), &(*col));
+    int size = (*row) * (*col);
+    *ar = new int[size];
+    printf("enter your %d data\n", size);
+    for (int i = 0; i < size; i++)
+    {
+        printf("[%d] ", i + 1);
+        scanf("%d", &(*ar)[i]);
+    }
+    return 0;
+}
