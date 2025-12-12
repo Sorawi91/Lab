4dbc940581c52468ef499198131c733318217251
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-13 00:27:12 +0700

    Update main.cpp

--- 2.3/main.cpp
+++ 2.3/main.cpp
@@ -9,9 +9,10 @@ int main() {
     char sprit = '/';
     char out [20][10];
     int num = 0;
-
+    
+    //กำหนดตัวเเปร
     explode(Ex,sprit,out,&num);
-
+    //เรียกใช้ funtionc 
     for (int i = 0 ; i < num ; i++ ) {
         printf("str2[%d] = \ %s \ \n",i,out[i]);
     }    
@@ -24,16 +25,19 @@ void explode (char str1[],char splitter , char str2[20][10] , int *count){
     int x = 0;
     int z = 0;
 
+    // loop for โดย ลูปตามตัวอักษรไปจนถึง \0 เเละเช็คว่ามันมีไม่มี \ ถ้าไม่มีให้ยัดลงไปใน str2
     for (int i = 0 ; str1[i] != '\0' ; i++) {
         if (str1[i] != splitter) {
             str2[x][z] = str1[i];
             z++;
-        } else {
+        } else { 
+            //ถ้าเจอ / ก็ให้ยัด \0 เข้าไปเเล้วเเละ ขยับตัวอักษรไปอีกตัวด้วย x เเละรีเซ็ตตำเเหน่งให้เป็น 0
             str2[x][z] = '\0';
             x++;
             z = 0;
         }
     }
+
     str2[x][z] = '\0';
 
     *count = x + 1;
