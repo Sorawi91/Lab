81e9c660d8d27592b597c5c3a7a3a257e5100489
DevC <60959635+Sorawi91@users.noreply.github.com>
2025-12-26 18:01:03 +0700

    Update main.c

--- 3.1/main.c
+++ 3.1/main.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
+
 struct student {
 	char name[ 20 ] ;
 	int age ;
 	char sex ;
 	float gpa ;
-} ;
-void upgrade( struct student *child ) ;
-int main() {
-	struct student aboy ;
-	aboy.sex = 'M' ;
-	aboy.gpa = 3.00 ;
-	upgrade( &aboy ) ;
-	printf( "%.2f", aboy.gpa ) ;
-	return 0 ;
-}
+};
+
 void upgrade( struct student *child ){
+
     if (child->sex == 'M') {
         child->gpa = child->gpa * 1.10;
         // 1.10 เท่ากับ 10%
@@ -22,4 +16,17 @@ void upgrade( struct student *child ){
         // 1.20 เท่ากับ 20%
         child->gpa = child->gpa * 1.20;
     }
+}
+
+int main() {
+	struct student aboy ;
+
+	aboy.sex = 'M' ;
+	aboy.gpa = 3.00 ;
+
+	upgrade( &aboy ) ;
+
+	printf( "%.2f", aboy.gpa ) ;
+
+	return 0 ;
 }
